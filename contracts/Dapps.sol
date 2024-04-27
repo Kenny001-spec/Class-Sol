4b8b99c2280c9b7dac3b21b4bc60c799566d1ccc
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-04-27 12:01:57 +0100

    contin

---
+++ contracts/Dapps.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Dapps {
+
+    uint storedData = 20;
+    string firstName = 'lucky';
+    bool isActive = true;
+
+    function set() {
+        //  statement block
+
+    }
+
+    function get() {
+        // statement block
+    }
+
+    function store() {
+        uint storage;
+
+        function set(uint x) public {
+            storage = x;
+        }
+    }
+}
